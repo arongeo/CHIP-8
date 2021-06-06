7354c2f89fe991436d467b7d00ec9da83cfb67e6
mrmalac <58132897+mrmalac@users.noreply.github.com>
2021-06-06 09:20:17 +0200

    Started working on virtual keyboard

---
+++ include/chip8_keyboard.h
@@ -0,0 +1,16 @@
+#ifndef CHIP8_KEYBOARD_H
+#define CHIP8_KEYBOARD_H
+
+#include <stdbool.h>
+#include "config.h"
+
+struct chip8_keyboard {
+	bool keyboard[CHIP8_TOTAL_KEYS];
+};
+
+int chip8_key_map(const char* map, int key);
+void chip8_key_down(struct chip8_keyboard* keyboard, int key);
+void chip8_key_up(struct chip8_keyboard* keyboard, int key);
+bool chip8_key_status(struct chip8_keyboard keyboard, int key); // If true key is down, if false key is up.
+
+#endif // CHIP8_KEYBOARD_H
